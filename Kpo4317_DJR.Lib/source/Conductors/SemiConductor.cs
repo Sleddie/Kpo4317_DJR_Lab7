3d0a2e4708ee64009c6d8ac1dcd4663b85c2709b
Sleddie <jdelmuhametov@gmail.com>
2021-05-04 00:38:11 +0300

    Initial commit

---
+++ Kpo4317_DJR.Lib/source/Conductors/SemiConductor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kpo4317_DJR.Lib
+{
+    public class SemiConductor
+    {
+        public SemiConductor()
+        {
+            Substance_name = "";
+            ForbiddenZone_width = 0;
+            Electron_move = 0;
+            Hole_move = 0;
+        }
+
+        public string Substance_name { get; set; }
+        public float ForbiddenZone_width { get; set; }
+        public int Electron_move { get; set; }
+        public int Hole_move { get; set; }
+    }
+}
---
+++ Kpo4317_DJR.Lib/source/Conductors/SemiconductorListTestLoader.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace Kpo4317_DJR.Lib
+{
+    public class SemiconductorListTestLoader : ISemiconductorListLoader
+    {
+        public SemiconductorListTestLoader()
+        {
+            this._semiconductorList = new List<SemiConductor>();
+        }
+
+        private List<SemiConductor> _semiconductorList = null;
+        private LoadStatus _status = LoadStatus.None;
+
+        public List<SemiConductor> semiconductorList { get { return _semiconductorList; } }
+
+        public LoadStatus status { get { return _status; } }
+
+        public OnLoadFileDelegate LoadingRow { get; set; }
+
+        public void Execute()
+        {
+            {
+                SemiConductor semiconductor = new SemiConductor()
+                {
+                    Substance_name = "Borum",
+                    ForbiddenZone_width = (float) 1.1,
+                    Electron_move = 10,
+                    Hole_move = 10
+                };
+                semiconductorList.Add(semiconductor);
+            }
+            {
+                SemiConductor semiconductor = new SemiConductor()
+                {
+                    Substance_name = "Tellur",
+                    ForbiddenZone_width = (float) 0.36,
+                    Electron_move = 1700,
+                    Hole_move = 1200,
+                };
+                semiconductorList.Add(semiconductor);
+            }
+            {
+                SemiConductor semiconductor = new SemiConductor()
+                {
+                    Substance_name = "CdTe",
+                    ForbiddenZone_width = (float) 1.45,
+                    Electron_move = 450,
+                    Hole_move = 100,
+                };
+                semiconductorList.Add(semiconductor);
+            }
+        }
+    }
+}
